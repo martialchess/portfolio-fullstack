e87d1b950ab90b8487f6944ea23871f9aafc71d4
Rida Malik <internalprogression@gmail.com>
2025-03-30 05:46:27 +0500

    Added working sendEmail controller using nodemailer

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { sendMail } = require("../controllers/emailController");
+const { sendEmail } = require("../controllers/emailController");
 
-router.post("/", sendMail);
+router.post("/", sendEmail);
 
 module.exports = router;
 // This code sets up an Express router for handling POST requests to the "/api/contact" endpoint. It imports the sendMail function from the emailController module and uses it to handle incoming requests. The router is then exported for use in other parts of the application.
