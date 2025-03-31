ccc8ee5992a3a89abc9eb9571713672be0c8776c
Rida Malik <internalprogression@gmail.com>
2025-03-31 05:57:07 +0500

    Cleaned up unused backend folder nested in frontend and confirmed functionality

--- pages/portfolio.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Portfolio = () => {
-  return <div>This is the Portfolio page</div>;
-};
-
-export default Portfolio;
--- pages/services.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Services = () => {
-  return <div>Our Services</div>;
-};
-
-export default Services;
-// Compare this snippet from my-portfolio/pages/index.tsx:
