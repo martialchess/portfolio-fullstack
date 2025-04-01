1e800297943403a5f2f030cad3f2a748f9618fa3
Rida Malik <internalprogression@gmail.com>
2025-04-01 06:28:42 +0500

    trigger deploy on render

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const contactRoute = require("./routes/contact");
+const contactRoute = require("./routes/contact"); //testing .env update
 
 const app = express();
 
